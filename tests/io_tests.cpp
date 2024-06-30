587ff64d9c0bc4255e8a70ac13812d9107bf396f
Jonathan Willianto <jowillianto@gmail.com>
2024-06-30 12:58:39 +0900

    [#main] Fix lock scoping

--- tests/io_tests.cpp
+++ tests/io_tests.cpp
@@ -38,7 +38,6 @@ auto writer_test = moderna::test_lib::make_tester("writer_tests")
                          auto writer = moderna::io::writable_file::open(WRITE_FILE).value();
                          writer.write("HELLO WORLD");
                          writer.close();
-                         l.unlock();
                          auto reader = moderna::io::readable_file::open(WRITE_FILE).value();
                          moderna::test_lib::assert_equal(reader.read().value(), "HELLO WORLD");
                        }
@@ -49,7 +48,6 @@ auto writer_test = moderna::test_lib::make_tester("writer_tests")
                        auto writer = moderna::io::writable_file::open(WRITE_FILE).value();
                        writer.writeline("HELLO WORLD");
                        writer.close();
-                       l.unlock();
                        auto reader = moderna::io::readable_file::open(WRITE_FILE).value();
                        moderna::test_lib::assert_equal(reader.read().value(), "HELLO WORLD\n");
                      });
