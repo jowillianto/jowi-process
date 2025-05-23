911e77ac54b2b352754f9566ab767792985eb40b
Jonathan Willianto <jowillianto@gmail.com>
2025-05-23 12:19:16 +0900

    [#main] added argument formatting

--- src/subprocess_argument.cc
+++ src/subprocess_argument.cc
@@ -1,4 +1,5 @@
 module;
+#include <format>
 #include <string>
 #include <type_traits>
 #include <vector>
@@ -24,6 +25,14 @@ namespace moderna::process {
       (__args.emplace_back(std::string{std::forward<Args>(args)}), ...);
       return *this;
     }
+    template <std::ranges::range range_type>
+      requires(std::is_constructible_v<std::string, std::ranges::range_value_t<range_type>>)
+    subprocess_argument &add_argument(range_type &&range) {
+      for (const auto &value : range) {
+        __args.emplace_back(std::string{value});
+      }
+      return *this;
+    }
     const char *const *args() const noexcept {
       __converted.clear();
       __converted.reserve(__args.size() + 2);
@@ -36,5 +45,26 @@ namespace moderna::process {
     const char *exec() const noexcept {
       return __exec.c_str();
     }
+
+    constexpr auto begin() const noexcept {
+      return __args.begin();
+    }
+    constexpr auto end() const noexcept {
+      return __args.end();
+    }
   };
+};
+
+namespace proc = moderna::process;
+template <class char_type> struct std::formatter<proc::subprocess_argument, char_type> {
+  constexpr auto parse(auto &ctx) {
+    return ctx.begin();
+  }
+  constexpr auto format(const proc::subprocess_argument &args, auto &ctx) const {
+    std::format_to(ctx.out(), "{} ", args.exec());
+    for (const auto &command : args) {
+      std::format_to(ctx.out(), "{} ", command);
+    }
+    return ctx.out();
+  }
 };
